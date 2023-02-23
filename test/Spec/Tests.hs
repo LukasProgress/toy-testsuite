21d9bb14a27d3b8e15591034dc79f66e69c71652
Lukas Probst <probst404@gmail.com>
2023-02-23 16:19:11 +0100

    Some more dependent tests

--- test/Spec/Tests.hs
+++ test/Spec/Tests.hs
@@ -1,6 +1,6 @@
 module Spec.Tests where
 
-import Test.Hspec
+import Test.Hspec ( it, shouldBe, Spec )
 
 biggerThan5 :: Monad m => Int -> m (Maybe Int, Spec)
 biggerThan5 n = let spec = it (show n ++ " is bigger than 5") $ n > 5 `shouldBe` True
@@ -19,3 +19,27 @@ onlyEvenBiggerThan5 n = let spec = it (show n ++ " is even and bigger than 5") $
                         in if n `div` 2 <= 5
                             then return (Just n, spec)
                             else return (Nothing, spec)
+
+
+
+-- Discussed example: 
+f1_modulo3 :: Monad m => Int -> m (Maybe Int, Spec)
+f1_modulo3 n = let m = mod n 3 
+                   spec = it (show n ++ " mod 3 is not odd") $ odd m `shouldBe` False
+               in if odd m
+                   then return (Nothing, spec)
+                   else return (Just m, spec)
+    
+f2_minus5 :: Monad m => Int -> m (Maybe Int, Spec)
+f2_minus5 n = let m = n - 5
+                  spec = it (show n ++ " minus 5 is not odd") $ odd m `shouldBe` False
+              in if odd m
+                    then return (Nothing, spec)
+                    else return (Just m, spec)
+
+productNotOdd :: Monad m => Int -> Int -> m (Maybe Int, Spec)
+productNotOdd n m = let o = m * n
+                        spec = it (show n ++ "*" ++ show m ++ " is not odd") $ odd o `shouldBe` False
+                    in if odd o
+                        then return (Nothing, spec)
+                        else return (Just o, spec)
--- test/TestRunner.hs
+++ test/TestRunner.hs
@@ -3,9 +3,7 @@ module Main where
 import Control.Monad.Except (forM)
 import Control.Monad (foldM)
 
-import Test.Hspec
-import Test.Hspec.Runner
-import Test.Hspec.Formatters
+import Test.Hspec ( hspec, describe, Spec )
 
 import Spec.Tests
 import Data.Maybe (fromJust, isJust)
@@ -14,17 +12,19 @@ import Data.List (transpose)
 type Description = String
 
 
-filterDependentTests :: Monad m => [[Maybe a]] -> m [Maybe a]
-filterDependentTests exs = return (map head . filter (all isJust) $ transpose exs)
+filterDependentTests :: Monad m => [[Maybe a]] -> m [Maybe [a]]
+filterDependentTests exs = return (foldr (zipWith (\x xs -> (:) <$> x <*> xs)) (repeat (Just [])) exs)
 
 runner :: Monad m
             => Description
             -> [[Maybe a]]
-            -> (a -> m (Maybe b, Spec))
+            -> ([a] -> m (Maybe b, Spec))
             -> m ([Maybe b], Spec)
 runner descr exs spectest = do
   dependencyFullfilled <- filterDependentTests exs
-  tested <- forM dependencyFullfilled $ \a -> spectest (fromJust a)
+  tested <- forM dependencyFullfilled $ \a -> case a of 
+                                                Nothing -> return (Nothing, pure ())
+                                                Just xs -> spectest xs
   sequenced <- foldM f ([], return ()) tested
   case sequenced of
     (bs, specs) -> return (bs, describe descr specs)
@@ -39,13 +39,21 @@ main :: IO ()
 main = do
     let examples = map Just $ fromTo 1 10
     print examples
-    (biggerThan5examples, biggerThan5spec) <- runner "Only examples bigger than 5" [examples] Spec.Tests.biggerThan5
+    (biggerThan5examples, biggerThan5spec) <- runner "Only examples bigger than 5" [examples] (\[ex] -> Spec.Tests.biggerThan5 ex)
 
-    (evenExamples, evenSpec) <- runner "Only even examples" [examples] Spec.Tests.onlyEven
+    (evenExamples, evenSpec) <- runner "Only even examples" [examples] (\[ex] -> Spec.Tests.onlyEven ex)
 
-    (evenAndBiggerThan5, dependentSpec) <- runner "Dependent on the first two tests: Bigger than 5 and even" [biggerThan5examples, evenExamples] Spec.Tests.onlyEvenBiggerThan5
+    -- Result of second dependency not used:
+    (evenAndBiggerThan5, dependentSpec) <- runner "Dependent on the first two tests: Bigger than 5 and even" [biggerThan5examples, evenExamples] (\[x1, _] -> Spec.Tests.onlyEvenBiggerThan5 x1)
+
+    -- Result of second dependecy used: 
+    (xs1, t1) <- runner "Numbers are not odd" [examples] (\[x] -> Spec.Tests.f1_modulo3 x)
+    (xs2, t2) <- runner "These Numbers are also not odd" [examples] (\[x] -> Spec.Tests.f2_minus5 x)
+    -- Use both dependencies: 
+    (productsNotOdd, doubleDependentSpec) <- runner "f (f1 x) (f2 x) not odd" [xs1, xs2] (\[x1, x2] -> Spec.Tests.productNotOdd x1 x2)
 
     hspec $ do
         -- biggerThan5spec -- 5 failures
         -- evenSpec        -- 5 failures
         dependentSpec      -- 0 failures, dependent on the first two
+        doubleDependentSpec
